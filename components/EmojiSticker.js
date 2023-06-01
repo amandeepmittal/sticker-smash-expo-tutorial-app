7f7f5bc368bfd534f0f53d773dce1e4afda13e8e
Aman Mittal <amandeepmittal@live.com>
2023-06-01 19:35:03 +0530

    Fix sticker scaling issue

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -20,7 +20,7 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 
   const onDoubleTap = useAnimatedGestureHandler({
     onActive: () => {
-      if (scaleImage.value) {
+      if (scaleImage.value !== imageSize * 2) {
         scaleImage.value = scaleImage.value * 2;
       }
     },
